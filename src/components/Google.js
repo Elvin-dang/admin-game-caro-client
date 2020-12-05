dba8acefafc3c1ba0ca689b555d04a62e8e7dda5
ntvinh1607 <btranmakemewonder@gmail.com>
2020-12-06 03:14:34 +0700

    authen

---
+++ src/components/Google.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+export default function Google(props) {
+	const { setIsRedirect } = props;
+	const responseGoogle = async(response) => {
+	  	if(response.profileObj!==undefined){
+			const user = { tokenId: response.tokenId };
+			await fetch(process.env.REACT_APP_api_domain+"/oauth/google", {
+				method: "POST",
+				headers: {'Content-Type':'application/json'},
+				body: JSON.stringify(user),
+			}).then(resp => resp.json()).then(data=>{
+			if(data.token){
+				localStorage.setItem('login', JSON.stringify({
+					login:true,
+					token:data.token,
+				}));
+				setIsRedirect();
+			}
+			else {
+				  alert("Please try again");
+				}
+			})
+		}
+	}
+	return (
+	  <GoogleLogin
+	    clientId={process.env.REACT_APP_ggid}
+	    buttonText="Login"
+	    onSuccess={responseGoogle}
+	    onFailure={responseGoogle}
+	    cookiePolicy={'single_host_origin'}
+	  />
+	)
+}
